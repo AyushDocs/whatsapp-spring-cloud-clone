895a4fc423e52a50ae436a9fd22ce8bb7b40fe51
AyushDocs <ayushalokdubey@gmail.com>
2022-03-25 21:03:41 +0300

    proper abstraction established over upload

--- image_service/src/main/java/com/whatsapp/image_service/services/ImageProcessor.java
+++ image_service/src/main/java/com/whatsapp/image_service/services/ImageProcessor.java
@@ -2,8 +2,12 @@ package com.whatsapp.image_service.services;
 
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.IOException;
 import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 
 import javax.imageio.ImageIO;
 
@@ -23,23 +27,27 @@ public class ImageProcessor {
     private String imagesDirectory;
 
     public void storeImageInDifferentFormats(MultipartFile file, String userId) {
-        if (file.getContentType().equals("image/jpeg"))
+        try {
             saveFile(file, userId, "png");
-        else if (file.getContentType().equals("image/png"))
             saveFile(file, userId, "jpeg");
+        } catch (IOException e) {
+            log.error("Could not save image file: " + file.getOriginalFilename() + " for userId: " + userId, e);
+        }
     }
 
-    private void saveFile(MultipartFile file, String userId, String fileType) {
-        try (InputStream is = file.getInputStream()) {
-            BufferedImage img = ImageIO.read(is);
-            String fileName = StringUtils.cleanPath(file.getOriginalFilename());
-            fileName=fileName.substring(0, fileName.lastIndexOf("."));
-            File filePath = Paths.get(imagesDirectory + userId + "/" + fileName + "." + fileType)
-                    .toFile();
-            ImageIO.write(img, fileType, filePath);
-        } catch (Exception e) {
-            log.error("Could not save image file: " + file.getOriginalFilename() + " for userId: " + userId + "type: "
-                    + fileType, e);
-        }
+    private void saveFile(MultipartFile multiPartfile, String userId, String fileType)
+            throws IOException {
+        String fileLocation = imagesDirectory + userId;
+        String fileName = StringUtils.cleanPath(multiPartfile.getOriginalFilename());
+        Path uploadPath = Paths.get(fileLocation);
+        Path filePath = uploadPath.resolve(fileName);
+        if (!Files.exists(uploadPath)) Files.createDirectories(uploadPath);
+        Files.copy(multiPartfile.getInputStream(), filePath, StandardCopyOption.REPLACE_EXISTING);
+        InputStream is = multiPartfile.getInputStream();
+        BufferedImage img = ImageIO.read(is);
+        fileName = fileName.substring(0, fileName.lastIndexOf("."));
+        File file = Paths.get(imagesDirectory + userId + "/" + fileName + "." + fileType)
+                .toFile();
+        ImageIO.write(img, fileType, file);
     }
 }
--- image_service/src/main/java/com/whatsapp/image_service/services/ImageService.java
+++ image_service/src/main/java/com/whatsapp/image_service/services/ImageService.java
@@ -1,11 +1,5 @@
 package com.whatsapp.image_service.services;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.nio.file.StandardCopyOption;
 import java.time.LocalDate;
 import java.util.List;
 
@@ -14,40 +8,28 @@ import com.whatsapp.image_service.models.ImageMetadata;
 import com.whatsapp.image_service.repositories.ImageMetadataRepo;
 import com.whatsapp.image_service.repositories.ImageRepo;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
 
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 
 @Service
 @RequiredArgsConstructor
-@Slf4j
 public class ImageService {
-    @Value("${image.upload.dir}")
-    private String imagesDirectory;
     private final ImageRepo imageRepo;
     private final ImageMetadataRepo imageMetadataRepo;
     private final ImageProcessor imageProcessor;
 
     public boolean storeImage(MultipartFile file, String userId) {
         String fileName = StringUtils.cleanPath(file.getOriginalFilename());
-        String fileLocation = imagesDirectory + userId;
-        Path uploadPath = Paths.get(fileLocation);
-        try (InputStream inputStream = file.getInputStream()) {
-            saveImageToFileSystem(fileName, uploadPath, inputStream);
-        } catch (IOException ioe) {
-            log.error("Could not save image file: " + fileName + " for userId: " + userId, ioe);
-            return false;
-        }
-        saveImageInDb(file, userId, fileName, fileLocation);
+        saveImageInDb(file, userId, fileName);
         imageProcessor.storeImageInDifferentFormats(file, userId);
         return true;
     }
 
-    private void saveImageInDb(MultipartFile file, String userId, String fileName, String fileLocation) {
+    private void saveImageInDb(MultipartFile file, String userId, String fileName) {
+        String fileLocation ="/api/v1/users/"+ userId+"/images/"+fileName;
         Image image = new Image();
         image.setUserId(userId);
         image.setImgName(fileName);
@@ -59,12 +41,5 @@ public class ImageService {
         image.setMetadata(List.of(imageMetadata));
         imageMetadataRepo.save(imageMetadata);
         imageRepo.save(image);
-    }
-
-    private void saveImageToFileSystem(String fileName, Path uploadPath, InputStream inputStream) throws IOException {
-        if (!Files.exists(uploadPath))
-            Files.createDirectories(uploadPath);
-        Path filePath = uploadPath.resolve(fileName);
-        Files.copy(inputStream, filePath, StandardCopyOption.REPLACE_EXISTING);
     }
 }
