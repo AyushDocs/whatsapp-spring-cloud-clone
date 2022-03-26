7c8e6af9f1835115cafb6316e5319b7587410ea6
AyushDocs <ayushalokdubey@gmail.com>
2022-03-26 10:44:08 +0300

    fixed image path

--- image_service/src/main/java/com/whatsapp/image_service/controllers/ImageController.java
+++ image_service/src/main/java/com/whatsapp/image_service/controllers/ImageController.java
@@ -23,12 +23,12 @@ import org.springframework.web.multipart.MultipartFile;
 import lombok.RequiredArgsConstructor;
 
 @RestController
-@RequestMapping("/api/v1/")
+@RequestMapping("/api/v1//images/{userId}")
 @RequiredArgsConstructor
 public class ImageController {
     private final ImageService imageService;
 
-    @PostMapping("/users/{userId}/images")
+    @PostMapping
     public ResponseEntity<?> uploadImage(@RequestParam("image") MultipartFile file,
             @PathVariable String userId) throws URISyntaxException {
         Image image = imageService.storeImage(file, userId);
@@ -36,7 +36,7 @@ public class ImageController {
         return ResponseEntity.created(new URI(path)).body(image);
     }
 
-    @GetMapping("/users/{userId}/images/{imageUrl}")
+    @GetMapping("/{imageUrl}")
     public ResponseEntity<?> findImage(@PathVariable String userId,@PathVariable String imageUrl){
         String imageInFileSystemPath = imageService.findImages(imageUrl, userId);
         if(imageInFileSystemPath == null) return ResponseEntity.badRequest().build();
