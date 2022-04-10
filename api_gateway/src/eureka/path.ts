6a1a6a65ecd3d868be941168643e1ea0b751b0c8
AyushDocs <ayushalokdubey@gmail.com>
2022-04-10 12:40:58 +0300

    implemented round robin

--- api_gateway/src/eureka/path.ts
+++ api_gateway/src/eureka/path.ts
@@ -4,15 +4,23 @@ import {EurekaClient} from 'eureka-js-client';
 import {Request} from 'express';
 import client from './client';
 /** @format */
+const map = new Map<string, number>();
 export default function getServiceUrl(req: Request, serviceName: string): string | undefined {
-	const instances = client.getInstancesByVipAddress(serviceName);
+	const instances = client.getInstancesByVipAddress(serviceName); //[profile_service]
 	if (!instances.length) return undefined;
-	if (!instances[0]) return undefined;
-	const port: any = extractPort(instances);
-	return 'http://' + instances[0].ipAddr + ':' + port + req.url;
+	const possibleValue = map.get(serviceName); //undefined
+	let port = extractPort(instances, 0),
+		index = 0;
+	if (!possibleValue || possibleValue > instances.length - 1) {
+		map.set(serviceName, 0);
+		return `http://${instances[index].ipAddr}:${port}${req.url}`;
+	}
+	port = extractPort(instances, possibleValue + 1);
+	map.set(serviceName, possibleValue + 1);
+	return `http://${instances[index].ipAddr}:${port}${req.url}`;
 }
-function extractPort(instances: EurekaClient.EurekaInstanceConfig[]) {
-	const portObj: any = instances[0]?.port;
+function extractPort(instances: EurekaClient.EurekaInstanceConfig[], index: number) {
+	const portObj: any = instances[index]?.port;
 	const port: any = portObj?.['$'];
 	return port;
 }
